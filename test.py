d538aa86a1b0d12a483e3ea032ba8a7a05ff08d5
selfiction <75582699+selfiction@users.noreply.github.com>
2022-06-08 01:52:19 +0500

    обновлённый, ворклист проделан

--- test.py
+++ test.py
@@ -9,7 +9,10 @@ import requests
 
 bot = Bot('a258ce79183e84321e497ba133ca1a7e19129308f548cacef46b9fefc88b2a8899410c383ce278c16c3b6')
 
-FILENAME = "image.jpg"
+FILENAMED = "image.jpg"
+FILENAMEQ = "image.jpg"
+
+
 
 @bot.on.message(text="d <f> <s>")
 async def readme_handler(m: Message, f, s):
@@ -18,13 +21,30 @@ async def readme_handler(m: Message, f, s):
     img_data = requests.get(urlpic).content
 
 
-    async with aiof.open(FILENAME, mode='wb') as out:
+    async with aiof.open(FILENAMED, mode='wb') as out:
 
         await out.write(img_data)
     dem = Demotivator(f, s)  # 2 строчки
     dem.create('image.jpg')
     photo = await PhotoMessageUploader(bot.api).upload(
         "demresult.jpg", peer_id=m.peer_id
+    )
+    await m.answer(attachment=photo)
+
+@bot.on.message(text="q <f>")
+async def readme_handler(m: Message, f):
+    users_info = await bot.api.users.get(m.from_id)
+    urlpic = m.attachments[0].photo.sizes[-5].url
+    print(urlpic)
+    img_data = requests.get(urlpic).content
+
+
+    async with aiof.open(FILENAMEQ, mode='wb') as out:
+        await out.write(img_data)
+    quot = Quote(f, author_name=users_info[0].first_name)  # 2 строчки
+    quot.create('image.jpg')
+    photo = await PhotoMessageUploader(bot.api).upload(
+        "qresult.png", peer_id=m.peer_id
     )
     await m.answer(attachment=photo)
 
